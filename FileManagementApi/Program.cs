4e04467bb30531b4866afad1f96b862f52218d6d
Serge Korolchuk <sergey.korolchuk.dev@gmail.com>
2018-10-09 23:27:18 +0300

    #1 FileManagementApi moved to new repo

---
+++ FileManagementApi/Program.cs
@@ -0,0 +1,16 @@
+﻿using Deeproxio.Infrastructure;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Deeproxio.FileManagementApi
+{
+    public class Program
+    {
+	    public static void Main(string[] args)
+	    {
+		    using (var server = new SelfHostServerBuilder<Startup>(args))
+		    {
+			    server.Build().Run();
+		    }
+	    }
+	}
+}
---
+++ FileManagementApi/Startup.cs
@@ -0,0 +1,35 @@
+﻿using Deeproxio.Infrastructure;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Deeproxio.FileManagementApi
+{
+    public class Startup : IServerStartup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
