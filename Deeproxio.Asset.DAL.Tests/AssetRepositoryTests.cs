3012fd8c8251a03a6804a02e0f20b35a6fdf435c
Serge Korolchuk <sergey.korolchuk.dev@gmail.com>
2020-07-27 23:40:22 +0300

    #12 Test projects preparation

--- Deeproxio.Asset.DAL.Tests/AssetRepositoryTests.cs
+++ Deeproxio.Asset.DAL.Tests/AssetRepositoryTests.cs
@@ -1,13 +1,22 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Diagnostics.CodeAnalysis;
+using Deeproxio.Asset.DAL.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace Deeproxio.Asset.DAL.Tests
 {
     [TestClass]
+    [ExcludeFromCodeCoverage]
     public class AssetRepositoryTests
     {
+        private MockRepository mockRepository;
+        private AssetRepository target;
+
         [TestInitialize]
         public void Initialize()
         {
+            mockRepository = new MockRepository(MockBehavior.Strict);
+            target = new AssetRepository();
         }
     }
 }
